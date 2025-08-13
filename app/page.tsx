ac999dc02e9b39c847bc9ec50c87922cf4a278ee
xk2312 <khayat@zencan.de>
2025-08-13 10:05:56 +0200

    deployment modification

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-// app/page.tsx
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 'use client'
 import { useEffect } from 'react';
 import CanvasMeteorAndM from './components/CanvasMeteorAndM'
