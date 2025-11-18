58a6603ae749f9fda360d7007c75b8b6f81e8921
xk2312 <khayat@zencan.de>
2025-11-18 16:50:17 +0100

    delete Footer Import

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -2,7 +2,6 @@
 
 import Navigation from "@/app/components/navigation/navigation";
 import { LegalPageShell } from "@/app/components/legal/LegalPage";
-import Footer from "@/app/components/subscription/footer";
 
 export default function PrivacyPage() {
   return (
