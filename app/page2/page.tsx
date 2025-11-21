ede85e7c38f7c70183633a3208fc3a7315e14ea0
xk2312 <khayat@zencan.de>
2025-11-21 09:37:32 +0100

    Säule 275 -> 278

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -1641,7 +1641,7 @@ return (
           display: "grid",
           gridTemplateColumns: isMobile
             ? "minmax(0,1fr)"
-            : "var(--saeule-w, 250px) minmax(0,1fr)",
+            : "var(--saeule-w, 278px) minmax(0,1fr)", // HIER SÄULENBREITE EINSTELLEN 1/2 Pltzen und (  :root { --dock-h: 60px; --fab-z: 90; --saeule-w: 275px; } )
           alignItems: "start",
           gap: 16,
 
@@ -1963,7 +1963,7 @@ return (
 
 
 
-  :root { --dock-h: 60px; --fab-z: 90; --saeule-w: 275px; }
+  :root { --dock-h: 60px; --fab-z: 90; --saeule-w: 278px; }
 
   .mi-plus-btn { display: none !important; }
 
