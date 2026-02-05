0ebe3a6fcaade79e6bed2091e1933ad798f5100d
xk2312 <khayat@zencan.de>
2026-02-06 00:28:47 +0100

    bugfix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -110,7 +110,7 @@ const base =
       value: sessionToken,
       httpOnly: true,
       sameSite: "lax",
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       path: "/",
       maxAge: 60 * 60 * 24 * 30, // 30 Tage
     });
